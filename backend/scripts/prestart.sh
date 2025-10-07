0208982e05f1708dac9ae59197d0d702453f7aa1
cn62025663 <ling.ye@celestica.com>
2025-10-07 18:59:38 +0800

    init commit

---
+++ backend/scripts/prestart.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+set -e
+set -x
+
+# Let the DB start
+python app/backend_pre_start.py
+
+# Run migrations
+alembic upgrade head
+
+# Create initial data in DB
+python app/initial_data.py
+
+# Initialize TimescaleDB configuration
+python app/init_timescale.py
